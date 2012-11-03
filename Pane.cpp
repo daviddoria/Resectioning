1ea43f627fd965e1b01fc027a611ee3759ad5405
David Doria <daviddoria@gmail.com>
2012-11-03 12:27:17 -0400

    Move these files out of PTXTools repo into their own repo.

---
+++ Pane.cpp
@@ -0,0 +1,23 @@
+#include "Pane.h"
+
+#include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
+#include <vtkSmartPointer.h>
+
+Pane::Pane(QVTKWidget* inputQVTKWidget)
+{
+  this->Renderer = vtkSmartPointer<vtkRenderer>::New();
+
+  this->qvtkWidget = inputQVTKWidget;
+  this->qvtkWidget->GetRenderWindow()->AddRenderer(this->Renderer);
+}
+
+Pane::~Pane()
+{
+
+}
+
+void Pane::Refresh()
+{
+  this->qvtkWidget->GetRenderWindow()->Render();
+}
